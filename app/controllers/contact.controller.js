f2475a1c5b87918aac37c22eaa07921dd0893d90
Ngo Phat Dat <ngodat1324@gmail.com>
2023-10-05 16:54:20 +0700

    final commit

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -57,22 +57,24 @@ exports.findOne = async (req, res, next) => {
 };
 
 exports.update = async (req, res, next) => {
-    if (Object.keys(req.body).length === 0 ) {
-        return next(new ApiError(400, "Data to update can not be empty"));
-    }
+    if (Object.keys(req.body).length === 0)
+    return next(new ApiError(400, "Data to update can not be empty"));
 
-    try {
-        const contactService = new ContactService(MongoDB.client);
-        const document = await contactService.update(req.params.id, req.body);
-        if (!document) {
-            return next(new ApiError(404, "Contact not found"));
-        }
-        return res.send({ message: "Contact was update successfully" });
-    } catch (error) {
-        return next(
-            new ApiError(500, `Error updating contact with id=${req.params.id}`)
-        );
-    }
+  try {
+    const contactService = new ContactService(MongoDB.client);
+    const document = await contactService.update(req.params.id, req.body);
+
+    console.log(document)
+
+    if (!document) return next(new ApiError(404, "Contact not found"));
+
+    return res.send({ message: "Contact was updated successfully" });
+  } catch (error) {
+    console.log(error);
+    return next(
+      new ApiError(500, `Error retrieving contact with id = ${req.params.id}`)
+    );
+  }
 };
 
 exports.delete = async (req, res, next) => {
